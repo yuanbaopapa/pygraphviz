909b07b7faa8b13ce77840e17c165311f42bc62b
Gabi Davar <grizzly.nyo@gmail.com>
2013-10-27 21:47:17 +0200

    make setup all os freindly again.

--- setup.py
+++ setup.py
@@ -28,8 +28,9 @@ if sys.version_info[:2] < (2, 4):
           sys.version_info[:2]
     sys.exit(-1)
 
-include_dirs=None
-library_dirs=None
+include_dirs = None
+library_dirs = None
+define_macros = []
 
 # If the setup script couldn't find your graphviz installation you can
 # specify it here by uncommenting these lines or providing your own:
@@ -54,19 +55,22 @@ library_dirs=None
 # Windows
 # Unknown - use command line -I and -L switches to set
 
-if sys.platform != "win32":
+if sys.platform == "win32":
+    define_macros.append(('GVDLL', None))
+
+else:
     # Attempt to find Graphviz installation
     if library_dirs is None and include_dirs is None:
         print "Trying pkg-config"
-        include_dirs,library_dirs=pkg_config()
+        include_dirs,library_dirs = pkg_config()
 
     if library_dirs is None and include_dirs is None:
         print "Trying dotneato-config"
-        include_dirs,library_dirs=dotneato_config()
+        include_dirs, library_dirs = dotneato_config()
 
     if library_dirs is None or include_dirs is None:
         print 
-        print  """Your Graphviz installation could not be found.
+        print """Your Graphviz installation could not be found.
 
     1) You don't have Graphviz installed:
        Install Graphviz (http://graphviz.org)
@@ -89,14 +93,14 @@ if sys.platform != "win32":
         print
         raise OSError,"Error locating graphviz."
 
-print "library_dirs=%s"%library_dirs
-print "include_dirs=%s"%include_dirs
+print "library_dirs=%s" % library_dirs
+print "include_dirs=%s" % include_dirs
 
 if library_dirs:
-    library_dirs=[library_dirs]
+    library_dirs = [library_dirs]
 
 if include_dirs:
-    include_dirs=[include_dirs]
+    include_dirs = [include_dirs]
 
 # Write the version information.
 sys.path.insert(0, 'pygraphviz')
@@ -104,42 +108,50 @@ import release
 release.write_versionfile()
 sys.path.pop(0)
 
-packages = ["pygraphviz","pygraphviz.tests"]
-docdirbase  = 'share/doc/pygraphviz-%s' % release.version
-data = [(docdirbase, glob("*.txt")),
-        (os.path.join(docdirbase, 'examples'),glob("examples/*.py")),
-        (os.path.join(docdirbase, 'examples'),glob("examples/*.dat")),
-        (os.path.join(docdirbase, 'examples'),glob("examples/*.dat.gz")),
-        ]
-extension = [Extension("pygraphviz._graphviz",
-                      ["pygraphviz/graphviz_wrap.c"],
-                      include_dirs=include_dirs,
-                      library_dirs=library_dirs,
-                      #runtime_library_dirs=library_dirs,
-                      libraries=["cgraph","cdt"],
-                      define_macros=[('GVDLL',None)],
-                      )]
-package_data = {'': ['*.txt'],}
+packages = ["pygraphviz", "pygraphviz.tests"]
+docdirbase = 'share/doc/pygraphviz-%s' % release.version
+data = [
+    (docdirbase, glob("*.txt")),
+    (os.path.join(docdirbase, 'examples'), glob("examples/*.py")),
+    (os.path.join(docdirbase, 'examples'), glob("examples/*.dat")),
+    (os.path.join(docdirbase, 'examples'), glob("examples/*.dat.gz")),
+]
+
+extension_args = {}
+if sys.platform != "win32":
+    extension_args['runtime_library_dirs'] = library_dirs
+
+extension = [
+    Extension(
+        "pygraphviz._graphviz",
+        ["pygraphviz/graphviz_wrap.c"],
+        include_dirs=include_dirs,
+        library_dirs=library_dirs,
+        libraries=["cgraph", "cdt"],
+        define_macros=define_macros,
+        **extension_args
+    )
+]
+
+package_data = {'': ['*.txt'], }
 
 if __name__ == "__main__":
-
-
     setup(
-        name             = release.name,
-        version          = release.version,
-        author           = release.authors['Hagberg'][0],
-        author_email     = release.authors['Hagberg'][1],
-        description      = release.description,
-        keywords         = release.keywords,
-        long_description = release.long_description,
-        license          = release.license,
-        platforms        = release.platforms,
-        url              = release.url,      
-        download_url     = release.download_url,
-        classifiers      = release.classifiers,
-        packages         = packages,
-        data_files       = data,
-        ext_modules      = extension,
-        package_data     = package_data
-      )
+        name=release.name,
+        version=release.version,
+        author=release.authors['Hagberg'][0],
+        author_email=release.authors['Hagberg'][1],
+        description=release.description,
+        keywords=release.keywords,
+        long_description=release.long_description,
+        license=release.license,
+        platforms=release.platforms,
+        url=release.url,
+        download_url=release.download_url,
+        classifiers=release.classifiers,
+        packages=packages,
+        data_files=data,
+        ext_modules=extension,
+        package_data=package_data
+    )
 
