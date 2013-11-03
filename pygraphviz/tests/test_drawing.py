d320391d5fba291a22329d467c92871b0dfa65b5
Aric Hagberg <aric.hagberg@gmail.com>
2013-11-02 21:11:29 -0600

    Comment out dubious test

--- pygraphviz/tests/test_drawing.py
+++ pygraphviz/tests/test_drawing.py
@@ -12,7 +12,8 @@ def test_drawing():
     A = pgv.AGraph(name='test graph')
     A.add_path([1,2,3,4])
     d = A.draw(prog='neato')
-    assert_equal(len(d.splitlines()),19)
+# this is probably not a good way to test
+#    assert_equal(len(d.splitlines()),19)
 # FIXME
 # smoke test
 # >>> (fd,fname)=tempfile.mkstemp()
